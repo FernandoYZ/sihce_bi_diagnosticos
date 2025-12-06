eb44b6820d6c1068a9bd404500e20b4f2d9b004e
FernandoYZ <fernandozavala266@gmail.com>
2025-12-06 07:26:29 -0500

    feat(config): agregar configuración de las variables de entorno para la DB

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import "os"
+
+type ConfigBD struct {
+	Usuario					string
+	Contrasena				string
+	Host					string
+	Puerto					string
+	NombreBD				string
+	Encrypt					string
+	TrustServerCertificate	string
+}
+
+func ConfiguracionBD() ConfigBD {
+	usuario := os.Getenv("DB_USER")
+	contrasena := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_SERVER")
+	puerto := os.Getenv("DB_PORT")
+	nombreBD := os.Getenv("DB_1")
+	encrypt := os.Getenv("DB_ENCRYPT")
+	trustServerCertificate := os.Getenv("DB_TRUST_SERVER_CERTIFICATE")
+
+	return ConfigBD{
+		Usuario:    usuario,
+		Contrasena: contrasena,
+		Host:       host,
+		Puerto:     puerto,
+		NombreBD:   nombreBD,
+		Encrypt:    encrypt,
+		TrustServerCertificate: trustServerCertificate,
+	}
+}
