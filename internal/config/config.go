51ada81828f17e948d57c65a7adfcafd98315559
FernandoYZ <fernandozavala266@gmail.com>
2025-12-07 17:31:25 -0500

    refactor(config): simplificar la estructura de la función de carga

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,32 +2,26 @@ package config
 
 import "os"
 
-type ConfigBD struct {
-	Usuario					string
-	Contrasena				string
-	Host					string
-	Puerto					string
-	NombreBD				string
-	Encrypt					string
-	TrustServerCertificate	string
+type Config struct {
+	ServerPort			   string
+	DBUser                 string
+	DBPassword             string
+	DBServer               string
+	DBPort                 string
+	DBName                 string
+	DBEncrypt              string
+	DBTrustServerCertificate string
 }
 
-func ConfiguracionBD() ConfigBD {
-	usuario := os.Getenv("DB_USER")
-	contrasena := os.Getenv("DB_PASSWORD")
-	host := os.Getenv("DB_SERVER")
-	puerto := os.Getenv("DB_PORT")
-	nombreBD := os.Getenv("DB_1")
-	encrypt := os.Getenv("DB_ENCRYPT")
-	trustServerCertificate := os.Getenv("DB_TRUST_SERVER_CERTIFICATE")
-
-	return ConfigBD{
-		Usuario:    usuario,
-		Contrasena: contrasena,
-		Host:       host,
-		Puerto:     puerto,
-		NombreBD:   nombreBD,
-		Encrypt:    encrypt,
-		TrustServerCertificate: trustServerCertificate,
+func CargarConfiguracion() Config {
+	return Config{
+		ServerPort:               os.Getenv("PORT"),
+		DBUser:                   os.Getenv("DB_USER"),
+		DBPassword:               os.Getenv("DB_PASSWORD"),
+		DBServer:                 os.Getenv("DB_SERVER"),
+		DBPort:                   os.Getenv("DB_PORT"),
+		DBName:                   os.Getenv("DB_1"),
+		DBEncrypt:                os.Getenv("DB_ENCRYPT"),
+		DBTrustServerCertificate: os.Getenv("DB_TRUST_SERVER_CERTIFICATE"),
 	}
 }
